fd56d3b91dfee920e908236522b36d3a01f68369
Satish Singh <satishsinghss1910@gmail.com>
2025-01-16 22:53:34 +0530

    added api and it auth

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,39 @@
+'use client'; 
+// import { IKImage, IKVideo, ImageKitProvider, IKUpload, ImageKitContext } from "imagekitio-next";
+import { IKUpload, ImageKitProvider } from "imagekitio-next";
 
+const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
+const authenticator = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/auth");
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(`Request failed with status ${response.status}: ${errorText}`);
+    }
+
+    const data = await response.json();
+    const { signature, expire, token } = data;
+    return { signature, expire, token };
+  } catch (error) {
+    throw new Error(`Authentication request failed: ${error.message}`);
+  }
+};
+
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 export default function Home() {
   return (
-<>
-hello
-</>
+    <div className="App">
+      <h1>ImageKit Next.js quick start</h1>
+      <ImageKitProvider publicKey={publicKey} urlEndpoint={urlEndpoint} authenticator={authenticator}>
+        <div>
+          <h2>File upload</h2>
+          <IKUpload fileName="test-upload.png" 
+          onError={(error) => console.error("Upload failed", error)} 
+          onSuccess={(response) => console.log("Upload successful", response)} 
+          />
+        </div>
+      </ImageKitProvider>
+    </div>
   );
 }
